5bf530def8bd4136fa898d38f86765de0750be7d
Maksym Radchenko <maksikchipic@gmail.com>
2025-12-10 12:56:02 +0100

    added styles to page

--- main.js
+++ main.js
@@ -84,3 +84,19 @@ function moveSlide(direction) {
     slider.style.transform = `translateX(-${index * imgWidth}px)`;
 }
 
+
+// accordion for guest designer
+var acc = document.getElementsByClassName("accordion")
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
