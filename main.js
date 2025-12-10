6236da2b1e2e05c1e1729bd350028fb37909d1b1
Maksym Radchenko <maksikchipic@gmail.com>
2025-12-10 14:54:10 +0100

    made accordion with animation

--- main.js
+++ main.js
@@ -86,17 +86,21 @@ function moveSlide(direction) {
 
 
 // accordion for guest designer
-var acc = document.getElementsByClassName("accordion")
-var i;
+var acc = document.getElementsByClassName("accordion");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (!panel) return;
+
+        panel.classList.toggle("open");
+
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;   // close
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";  // open
+        }
+    });
 }
